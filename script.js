2844a28233e7ac5fcb367e8e9e9aa923da967273
Vitor-Maia-pw <vitormaiatls@gmail.com>
2025-07-30 13:33:58 -0400

    função para mudar de fundo

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ function abrirCategorias() {
   if (abreCate === 0) {
     setaCate.style.transform = "rotate(-180deg)"
     abrindoCategorias()
-  } else{
+  } else {
     setaCate.style.transform = "rotate(0deg)"
     fechandoCategorias()
-  } 
+  }
 }
 
 //funções de animação para as categorias
@@ -34,7 +34,7 @@ function abrindoCategorias() {
     categoriasT += 1.01
     categorias.style.top = categoriasT + "vw"
     requestAnimationFrame(abrindoCategorias)
-  } else{
+  } else {
     abreCate = 1 //categorias agora está visivel para o usuario
   }
 }
@@ -43,8 +43,59 @@ function fechandoCategorias() {
     categoriasT += -1
     categorias.style.top = categoriasT + "vw"
     requestAnimationFrame(fechandoCategorias)
-  } else{
+  } else {
     abreCate = 0 //categorias agora está visivel para o usuario
+  }
+}
+
+// função para quando a pessoa escolher qual tipo de peça ela quer nas categorias
+function pecaEscolhida(peca) {
+  const fundo1 = document.querySelector("#fundo1")
+  const fundo2 = document.querySelector("#fundo2")
+  let nomeFundo2 = document.querySelector("#nomeFundo2")
+  switch (peca) {
+    case 1:
+      fundo1.style.display = "none"
+      fundo2.style.display = "flex"
+      abrirCategorias()
+      nomeFundo2.innerHTML = "Corpo de Válvulas"
+      break;
+    case 2:
+      fundo1.style.display = "none"
+      fundo2.style.display = "flex"
+      abrirCategorias()
+      nomeFundo2.innerHTML = "Conversores de Torque"
+
+      break;
+    case 3:
+      fundo1.style.display = "none"
+      fundo2.style.display = "flex"
+      abrirCategorias()
+      nomeFundo2.innerHTML = "Transmissões Completas"
+
+      break;
+    case 4:
+      fundo1.style.display = "none"
+      fundo2.style.display = "flex"
+      abrirCategorias()
+      nomeFundo2.innerHTML = "Módulo de Controle de Transmissão"
+
+      break;
+    case 5:
+      fundo1.style.display = "none"
+      fundo2.style.display = "flex"
+      abrirCategorias()
+      nomeFundo2.innerHTML = "Carcaças"
+
+      break;
+    case 6:
+      fundo1.style.display = "none"
+      fundo2.style.display = "flex"
+      fechandoCategorias()
+      nomeFundo2.innerHTML = "Outras Peças de Transmissão"
+
+      break;
+
   }
 }
 
